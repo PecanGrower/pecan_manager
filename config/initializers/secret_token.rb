b46e7892d2d863d0ef14b2e1e3a2c3ccf1c0f76c
Greg Daviet <Greg@theDaviets.com>
2011-09-10 16:27:17 -0600

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PecanManager::Application.config.secret_token = 'cf1110291422ecdc3813215ab8eaaf499c378ffe0f8106cb528ede76892913bd374c8e330f7d45e594c3c723b84f5bd35f252eeaa9eb1812e5ff916187796305'
